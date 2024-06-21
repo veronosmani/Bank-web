05ad4a83bf59cd4aa920d1ffcee629e3f25c63dd
veronosmani <veron.osmani@gmail.com>
2024-06-21 16:09:33 +0200

    modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,10 @@ import {discount, robot} from '../assets'
 const Hero = () => {
   return (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div>
-      
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:p-0 sm:px-16 px-6`}>
+        <div>
+          
+        </div>
       </div>    
     </section>
   )
