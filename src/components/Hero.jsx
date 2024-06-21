e82feba2aed1fdaa42095973589e5d472489aa02
veronosmani <veron.osmani@gmail.com>
2024-06-21 16:06:34 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
    On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from '../style'
-
+import {discount, robot} from '../assets'
 const Hero = () => {
   return (
-    <section id='home' className={``}>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div>
       
+      </div>    
     </section>
   )
 }
