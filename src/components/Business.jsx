03e09809ce9a51f84b65b7ac9f1318d4c6c6a072
veronosmani <veron.osmani@gmail.com>
2024-06-24 09:47:50 +0200

    modified:   src/components/Business.jsx 	modified:   src/components/Button.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,12 +2,26 @@ import {features} from '../constants'
 import styles, {layout} from '../style'
 import Button from './Button'
 
+const FeatureCard = ({icon, title, content, index}) => {
+  <div>
+    <div>
+      <img />
+    </div>
+  </div>
+}
+
 const Business = () => {
   return (
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>You so the buisness,<br className='sm:block hidden' /> we'll handle the money.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life and by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <Button styles='mt-10' />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
       </div>
     </section>
   )
